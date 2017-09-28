e7f2ea243fecf9742de79fc1e96cbe34324699d1
Hoon Lee <yl.hoony@gmail.com>
2017-09-28 08:53:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,24 +35,3 @@ function calculatesFarePrice(start, destination) {
     return 'cannot travel that far';
   }
 }
-
-
-//
-// describe('calculatesFarePrice(start, destination)', function () {
-//   it('gives customers a free sample', function () {
-//     expect(calculatesFarePrice(43, 44)).to.equal(0);
-//   });
-//
-//   it('charges 2 cents per foot (and does not give any discount on the first 200 feet) when total feet travelled between 400 and 2000 in example below is 528', function () {
-//     // total feet travelled is 528 -> 10.56
-//     expect(calculatesFarePrice(34, 32)).to.equal(10.56);
-//   });
-//
-//   it('charges 25 dollars for a distance over 2000 feet', function () {
-//     expect(calculatesFarePrice(50, 58)).to.equal(25);
-//   });
-//
-//   it('does not allow rides over 2500 feet', function () {
-//     expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');
-//   });
-// });
