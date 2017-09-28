ade5f4dba5bdfe6f349f3fe3a07c843247174125
Hoon Lee <yl.hoony@gmail.com>
2017-09-28 08:46:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,20 +18,10 @@ function distanceFromHqInFeet(location) {
   return distanceFromHqInBlocks(location) * 264;
 }
 
-//
-// describe('distanceFromHqInFeet()', function () {
-//   it('returns a distance in feet', function () {
-//     expect(distanceFromHqInFeet(43)).to.equal(264);
-//   });
-//
-//   it('returns a distance in feet', function () {
-//     expect(distanceFromHqInFeet(50)).to.equal(2112);
-//   });
-//
-//   it('calculates distances below 42nd street', function () {
-//     expect(distanceFromHqInFeet(34)).to.equal(2112);
-//   });
-// });
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+  return Math.abs(endingBlock - startingBlock) * 264;
+}
+
 //
 // describe('distanceTravelledInFeet()', function () {
 //   it('returns the distance travelled in feet', function () {
