04d52d30a0234b0b5f89562a71b3e43b603227e1
Hoon Lee <yl.hoony@gmail.com>
2017-09-28 08:53:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function calculatesFarePrice(start, destination) {
     return 0;
   } else if (distance < 2000) {
     return distance * 0.02;
-  } else if (2500) {
+  } else if (distance < 2500) {
     return 25;
   } else {
     return 'cannot travel that far';
